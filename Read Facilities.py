fb823156e0d58424c183e73cea9f3444481f9a87
Wintersxx <andersonkyle099@gmail.com>
2022-04-12 14:22:02 -0600

    Update Read Facilities.py

--- Read Facilities.py
+++ Read Facilities.py
@@ -17,7 +17,7 @@ start_date = str(input("Start date for this budget period \"YYYY-MM-DD\": "))
 budgets_sent = int(input("How many budgets were sent out?: "))
 print("*"* 60)
 
-path = fr"P:\PACS\Finance\Budgets\{folder}\Received\Final Q1\*"
+path = fr"P:\PACS\Finance\Budgets\{folder}\Received\*"
 
 
 final_wb = xw.Book()
@@ -25,30 +25,39 @@ final_wb = xw.Book()
 x = 2
 for file in glob(path):
     wb = xw.Book(file,update_links=False)
-    full_budget = wb.sheets("RPT - ALL Lines")                  #open tab
+    full_budget = wb.sheets("RPT - ALL Lines")                      #open tab
     facility_info = wb.sheets("FACILITY INFO")
+    main_page = wb.sheets("BUDGET WORKSHEET")
+
+    dates = full_budget.range("C5:O5")                              # pull all dates
+    noi = full_budget.range("C184:O184")                            #NOI for all dates
     budget_start_date = facility_info.range("B15")
-    dates = full_budget.range("C5:O5")                          #pull all dates
-    noi = full_budget.range("C184:O184")                        #NOI for all dates
+    bed_ct = facility_info.range("B10")
+    occupancy_rate = main_page.range("J2")
 
 
-    Facility_name = wb.sheets("FACILITY INFO").range("B7").value     #pull Facility
-    NOI = wb.sheets("BUDGET WORKSHEET").range("I3").value       #pull NOI value
+    Facility_name = wb.sheets("FACILITY INFO").range("B7").value    #pull Facility
+    NOI = wb.sheets("BUDGET WORKSHEET").range("I3").value           #pull NOI value
 
 
     final_wb.sheets[0].range(f"P{x}").value = budget_start_date.value
-    final_wb.sheets[0].range("C1:O1").value = dates.value       #
-    final_wb.sheets[0].range(f"C{x}:O{x}").value = noi.value    #drop in dates
+    final_wb.sheets[0].range(f"Q{x}").value = bed_ct.value
+    final_wb.sheets[0].range(f"R{x}").value = occupancy_rate.value
 
-    final_wb.sheets[0].range(f"A{x}").value = Facility_name          #drop in Facility name in new workbook
-    final_wb.sheets[0].range(f"B{x}").value = NOI               #drop in NOI value in workbook
+
+    final_wb.sheets[0].range("C1:O1").value = dates.value
+    final_wb.sheets[0].range(f"C{x}:O{x}").value = noi.value        #drop in dates
+
+    final_wb.sheets[0].range(f"A{x}").value = Facility_name         #drop in Facility name in new workbook
+    final_wb.sheets[0].range(f"B{x}").value = NOI                   #drop in NOI value in workbook
     x += 1
     wb.close()
 
-
-final_wb.sheets[0].range(f"p1").value = 'Budget Start Date'
 final_wb.sheets[0].range(f"a1").value = 'Facility'
-final_wb.sheets[0].range(f"B1").value = 'NOI'
+final_wb.sheets[0].range(f"b1").value = 'NOI'
+final_wb.sheets[0].range(f"p1").value = 'Budget Start Date'
+final_wb.sheets[0].range(f"q1").value = 'Bed Count'
+final_wb.sheets[0].range(f"r1").value = 'Occupancy Rate'
 
 final_wb.save(fr"P:\PACS\Finance\Budgets\{folder}\budgets checked.xlsx")
 
